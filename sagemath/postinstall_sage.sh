0cc8b3668f6f79b78016014c8900569cb4c14f85
Nicolas M. Thiéry <nthiery@users.sf.net>
2016-01-24 10:45:57 +0000

    Updated the two copies of the postinstall script

--- sagemath/postinstall_sage.sh
+++ sagemath/postinstall_sage.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-sudo ./sage --nodotsage <<EOFSAGE
-    install_scripts("/usr/bin")
-EOFSAGE
+cd /opt/sage
 
+# Put scripts to start gap, gp, maxima, ... in /usr/bin
+sudo sage --nodotsage -c "install_scripts('/usr/bin')"
+
+# Add aliases for sage and sagemath
+sudo ln -sf /opt/sage/sage /usr/bin/sage
+sudo ln -sf /opt/sage/sage /usr/bin/sagemath
+
+# Setup the admin password for Sage's lecacy notebook to avoid need for later user interaction
 ./sage <<EOFSAGE
     from sage.misc.misc import DOT_SAGE
     from sagenb.notebook import notebook
@@ -13,5 +19,3 @@ EOFSAGE
     nb.save()
     quit
 EOFSAGE
-
-sudo ln -sf /opt/sage/sage /usr/bin/sagemath
