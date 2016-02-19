2e5e6cf1d43c4152af16df1aa8106f3db456e96a
Erik M. Bray <erik.bray@lri.fr>
2016-02-19 10:55:51 +0100

    Reworked Sage installation script to be shared between sagemath and sagemath-develop images, reducing the duplication between them.  Improved the Makeful to clarify which files are generated and should be ignored by git.

---
+++ common/install_sage.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# !!!NOTE!!! This script is intended to be run with root privileges
+# It will run as the 'sage' user when the time is right.
+SAGE_SRC_TARGET=${1%/}
+BRANCH=$2
+
+if [ -z $SAGE_SRC_TARGET ]; then
+  >&2 echo "Must specifiy a target directory for the sage source checkout"
+  exit 1
+fi
+
+if [ -z $BRANCH ]; then
+  >&2 echo "Must specify a branch to build"
+  exit 1
+fi
+
+N_CORES=$(cat /proc/cpuinfo | grep processor | wc -l)
+
+export SAGE_FAT_BINARY="yes"
+export MAKE="make -j${N_CORES}"
+cd "$SAGE_SRC_TARGET"
+git clone --depth 1 --branch ${BRANCH} https://github.com/sagemath/sage.git
+chown -R sage:sage sage
+cd sage
+
+# Sage can't be built as root, for reasons...
+sudo -H -E -u sage make || exit 1
+
+# Put scripts to start gap, gp, maxima, ... in /usr/bin
+./sage --nodotsage -c "install_scripts('/usr/bin')"
+
+# Add aliases for sage and sagemath
+ln -sf "${SAGE_SRC_TARGET}/sage/sage" /usr/bin/sage
+ln -sf "${SAGE_SRC_TARGET}/sage/sage" /usr/bin/sagemath
+
+# Setup the admin password for Sage's lecacy notebook to avoid need for later user interaction
+./sage <<EOFSAGE
+    from sage.misc.misc import DOT_SAGE
+    from sagenb.notebook import notebook
+    directory = DOT_SAGE+'sage_notebook'
+    nb = notebook.load_notebook(directory)
+    nb.user_manager().add_user('admin', 'sage', '', force=True)
+    nb.save()
+    quit
+EOFSAGE
+
+# Clean up artifacts from the sage build that we don't need for runtime or
+# running the tests
+#
+# Unfortunately none of the existing make targets for sage cover this ground
+# exactly
+make misc-clean
+make -C src/ clean
+rm -rf upstream/
+rm -rf src/doc/output/doctrees/
