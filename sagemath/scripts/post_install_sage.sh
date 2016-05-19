7ef62dc88491a57918b911ae107675d6da5ee2f1
Erik M. Bray <erik.bray@lri.fr>
2016-05-19 14:32:24 +0000

    Don't use ./sage here since this script is not necessarily run from SAGE_TARGET

--- sagemath/scripts/post_install_sage.sh
+++ sagemath/scripts/post_install_sage.sh
@@ -4,13 +4,13 @@
 # rebuild.
 
 # Put scripts to start gap, gp, maxima, ... in /usr/bin
-./sage --nodotsage -c "install_scripts('/usr/bin')"
+sage --nodotsage -c "install_scripts('/usr/bin')"
 
 # Setup the admin password for Sage's lecacy notebook to avoid need for later
 # user interaction
 # This should also be run as the 'sage' user to ensure that the resulting
 # configuration is written to their DOT_SAGE
-sudo -H -u sage ./sage <<EOFSAGE
+sudo -H -u sage sage <<EOFSAGE
     from sage.misc.misc import DOT_SAGE
     from sagenb.notebook import notebook
     directory = DOT_SAGE+'sage_notebook'
@@ -22,4 +22,4 @@ EOFSAGE
 
 # Install additional Python packages into the sage Python distribution...
 # Install terminado for terminal support in the Jupyter Notebook
-sudo -H -u sage ./sage -pip install terminado
+sudo -H -u sage sage -pip install terminado
