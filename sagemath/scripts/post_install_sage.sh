acbfc7d54d435cf11b6325e93eab2c3dfe9ee48c
Erik M. Bray <erik.bray@lri.fr>
2017-11-09 12:51:11 +0000

    My original idea of filling out /etc/environment didn't work.
    This approach seems a bit more robust, if not still a bit ugly.  It adds
    a sage-entrypoint which, when used as the ENTRYPOINT in the Dockerfile,
    sets all the default environment variables for the 'sage' user.
    
    Also changed things slightly so that the post_install.sh script is not
    run as root by default.

--- sagemath/scripts/post_install_sage.sh
+++ sagemath/scripts/post_install_sage.sh
@@ -1,16 +1,23 @@
+#!/bin/bash
 # Additional setup to perform after building and installing Sage
 # This is broken out into a separate script so it can be run as
 # a separate step in the Dockerfile without performing a full
-# rebuild.
+# rebuild.  This script is run as the 'sage' user.
+
+SAGE_SRC_TARGET=${1%/}
+if [ -z $SAGE_SRC_TARGET ]; then
+  >&2 echo "Must specify a target directory for the sage source checkout"
+  exit 1
+fi
 
 # Put scripts to start gap, gp, maxima, ... in /usr/bin
-sage --nodotsage -c "install_scripts('/usr/bin')"
+sudo sage --nodotsage -c "install_scripts('/usr/bin')"
 
 # Setup the admin password for Sage's lecacy notebook to avoid need for later
 # user interaction
 # This should also be run as the 'sage' user to ensure that the resulting
 # configuration is written to their DOT_SAGE
-sudo -H -u sage sage <<EOFSAGE
+sage <<EOFSAGE
     from sage.misc.misc import DOT_SAGE
     from sagenb.notebook import notebook
     directory = DOT_SAGE+'sage_notebook'
@@ -24,12 +31,30 @@ EOFSAGE
 # Install terminado for terminal support in the Jupyter Notebook
 # Upgrade Jupyter notebook to 5.x
 # Install additional Jupyter kernels
-sudo -H -u sage sage -pip install \
+sage -pip install \
     terminado \
     notebook>=5 \
     ipykernel>=4.6
 
-sudo -H -u sage sage -i \
+sage -i \
     gap_jupyter \
     singular_jupyter \
     pari_jupyter
+
+
+# Generate the sage-entrypoint.sh script which sets the sage environment for
+# all commands run in the container; first we output 'set' in the default
+# environment, then 'set' in the sage environment and take only those settings
+# that are added by the sage environment
+bash -c set | sort > /tmp/orig_env.txt
+sage -sh -c set | sort > /tmp/sage_env.txt
+envvars=$(diff /tmp/orig_env.txt /tmp/sage_env.txt | grep '^> ' | grep -v '^SHLVL=\|^_=' | cut -d' ' -f2-)
+envvars=$(echo "$envvars" | sed 's/^/export /')
+cat > "$SAGE_SRC_TARGET/sage/local/bin/sage-entrypoint.sh" <<_EOF_
+#!/bin/bash
+${envvars}
+exec "\$@"
+_EOF_
+chmod +x "$SAGE_SRC_TARGET/sage/local/bin/sage-entrypoint.sh"
+rm -f /tmp/*_env.txt
+sudo ln -s "$SAGE_SRC_TARGET/sage/local/bin/sage-entrypoint.sh" /usr/local/bin/sage-entrypoint
